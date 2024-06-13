6ba534ad93c1c2381f78a68be667fcad90d4dcd8
Neelima-6364 <163846079+Neelima-6364@users.noreply.github.com>
2024-06-13 21:34:33 +0530

    org

--- VTIGER/src/test/java/com/vtiger/crm/orgtest/OrganizationTest.java
+++ VTIGER/src/test/java/com/vtiger/crm/orgtest/OrganizationTest.java
@@ -1,12 +1,48 @@
 package com.vtiger.crm.orgtest;
 
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class OrganizationTest {
-	@Test
-	public void mTest()
+import com.aventstack.extentreports.Status;
+import com.crm.BaseClass.BaseClass;
+import com.vtiger.crm.generic.webdriverutility.UtilityClassobject;
+import com.vtiger.crm.objectrepositoryutility.CreateNewOrganizationPage;
+import com.vtiger.crm.objectrepositoryutility.HomePage;
+import com.vtiger.crm.objectrepositoryutility.OrganizationInformationPage;
+import com.vtiger.crm.objectrepositoryutility.OrganizationsPage;
+
+public class OrganizationTest extends BaseClass {
+	@Test(groups = {"smokeTest"})
+	public void createOrgWithMandField() throws EncryptedDocumentException, IOException
 	{
-		System.out.println("create organization");
-	}
+		UtilityClassobject.getTest().log(Status.INFO, "Read data from Excel");
+		
+		/*get test data from excel*/
+		String orgName = eLib.getDataFromExcelFile("Org", 1, 2)+ jLib.getRandomNumber();
+
+		/*Navigate to Organization Module*/
+		HomePage hp=new HomePage(driver);
+		UtilityClassobject.getTest().log(Status.INFO, "Navigate to Organization List page");
+		hp.getOrganizationLink().click();
+		
+		OrganizationsPage orgp=new OrganizationsPage(driver);
+		UtilityClassobject.getTest().log(Status.INFO, "Navigate to create Organization page");
+		orgp.getCreateOrgBtn().click();
+		
+		/* create organization with organization name*/
+		CreateNewOrganizationPage crneworgp=new CreateNewOrganizationPage(driver);
+		UtilityClassobject.getTest().log(Status.INFO, "Enter OrgName and create Organization");
+		crneworgp.createOrganization(orgName);
+
+		/*verify header oganisation actual data*/
+		OrganizationInformationPage orginfop=new OrganizationInformationPage(driver);
+		String headerinfo =orginfop.getHeaderinfo().getText();
+
+		Assert.assertEquals(true, headerinfo.contains(orgName));
+		UtilityClassobject.getTest().log(Status.INFO,"Header Info verified");
+	}	
 
 }
