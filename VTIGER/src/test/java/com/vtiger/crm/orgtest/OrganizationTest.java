6dd2d79c43d64d68839d9419e7d0eeb5ace0bd18
Neelima-6364 <163846079+Neelima-6364@users.noreply.github.com>
2024-06-13 21:09:48 +0530

    changed

---
+++ VTIGER/src/test/java/com/vtiger/crm/orgtest/OrganizationTest.java
@@ -0,0 +1,12 @@
+package com.vtiger.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test
+	public void mTest()
+	{
+		System.out.println("create organization");
+	}
+
+}
